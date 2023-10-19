551544d3d89fc42840e95e950d4baa1580f79ca6
josue11112 <tvjosue090@gmail.com>
2023-10-19 15:39:41 -0500

    contactanos

--- enviar.php
+++ enviar.php
@@ -1,10 +1,14 @@
 <?php
+if (isset($_POST["enviar"])) {
     // Recoge los datos del formulario
     $nombre_completo = $_POST['nombre_completo'];
     $correo_institucional = $_POST['correo_institucional'];
     $contacto_celular = $_POST['contacto_celular'];
     $cedula = $_POST['cedula'];
     $conocimiento_microbiologia = $_POST['conocimiento_microbiologia'];
+    $practicas_microorganismos = $_POST['practicas_microorganismos'];
+    $capacitacion_necesaria = $_POST['capacitacion_necesaria'];
+    $mensaje_usuario = $_POST['mensaje'];
 
     // Define a quién se enviará el correo
     $para = 'tvjosue090@gmail.com';
@@ -13,24 +17,26 @@
     $asunto = 'nuevo contacto';
 
     // Construye el cuerpo del correo
-    $mensaje = "Este mensaje fue enviado por: " . $nombre_completo . "\r\n";
-    $mensaje .= "Su correo institucional es: " . $correo_institucional . "\r\n";
-    $mensaje .= "Contacto celular: " . $contacto_celular . "\r\n";
+    $mensaje = "Este mensaje fue enviado por: " . $nombre_completo . "\n";
+    $mensaje .= "Su correo institucional es: " . $correo_institucional . "\n";
+    $mensaje .= "Contacto celular: " . $contacto_celular . "\n";
     $mensaje .= "Cédula: " . $cedula . "\r\n";
-    $mensaje .= "Conocimiento sobre microbiología: " . $conocimiento_microbiologia . "\r\n";
+    $mensaje .= "Conocimiento sobre microbiología: " . $conocimiento_microbiologia . "\n";
+    $mensaje .= "Ha realizado prácticas con microorganismos: " . $practicas_microorganismos . "\n";
+    $mensaje .= "Considera necesaria una capacitación práctica sobre la manipulación de microorganismos al inicio de su vinculación en el proyecto: " . $capacitacion_necesaria . "\n";
+    $mensaje .= "Mensaje del usuario: " . $mensaje_usuario . "\n";
 
     // Define las cabeceras del correo
-    $header = 'From: ' . $correo_institucional . "\r\n";
-    $header .= "X-Mailer: PHP/" . phpversion() . "\r\n";
-    $header .= "Mime-Version: 1.0 \r\n";
-    $header .= "Content-Type: text/plain";
+    $header = 'From: ' . $correo_institucional . "\n";
+
 
     // Envía el correo
     $enviado = mail($para, $asunto, $mensaje, $header);
 
     if ($enviado) {
-        echo 'El correo se ha enviado correctamente.';
+        echo "<script>alert('El correo se ha enviado correctamente.');</script>";
     } else {
-        echo 'Hubo un error al enviar el correo.';
+        echo "<script>alert('El correo no se ha enviado.');</script>";
     }
+}
 ?>
